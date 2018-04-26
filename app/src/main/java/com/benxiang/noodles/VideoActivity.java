e795de5c3e22a0217b2bc0c5c2507d136fbc1d2a
emian1.0 <303270656@qq.com>
2018-04-26 15:55:59 +0800

    修改宣传界面的视频播放时间设置，晚上22点之后，和早上8点之前无声播放。每35秒发起一次获取本地时间

--- app/src/main/java/com/benxiang/noodles/VideoActivity.java
+++ app/src/main/java/com/benxiang/noodles/VideoActivity.java
@@ -1,29 +1,41 @@
 package com.benxiang.noodles;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.MediaController;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 import android.widget.VideoView;
+
+import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class VideoActivity extends AppCompatActivity {
     VideoView videoView;
     RelativeLayout relativeLayout;
+    protected String format;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
         videoView = (VideoView) findViewById(R.id.videoView);
         relativeLayout = (RelativeLayout) findViewById(R.id.videoActivity);
-        //初始化 videoView
-        initVideoView();
+        new Thread(new TimeThread()).start();
     }
-    private void initVideoView() {
+
+    private void initVideoView(final int time) {
         {
             try{
                 MediaController mc = new MediaController(this);
@@ -46,6 +58,9 @@ public class VideoActivity extends AppCompatActivity {
                 videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
                     @Override
                     public void onPrepared(MediaPlayer mediaPlayer) {
+                        if (time >= 22 || time <= 8){//|| mTime < 8
+                            mediaPlayer.setVolume(0f, 0f);
+                        }
                         mediaPlayer.start();
                         mediaPlayer.setLooping(true);
                     }
@@ -75,6 +90,44 @@ public class VideoActivity extends AppCompatActivity {
             if(videoView!=null){
                 videoView.suspend();
             }
+        }
+    }
+
+
+    @SuppressLint("HandlerLeak")
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case 1:
+                    long time = System.currentTimeMillis();
+                    Date date = new Date(time);
+//                    SimpleDateFormat format = new SimpleDateFormat("yyyy年MM月dd日 HH时mm分ss秒 EEEE");
+                    SimpleDateFormat format = new SimpleDateFormat("HH");
+                    String data = format.format(date);
+                    int mTime = Integer.parseInt(data);
+                    initVideoView(mTime);
+                    break;
+                default:
+                    break;
+
+            }
+        }
+    };
+    class TimeThread extends Thread {
+        @Override
+        public void run() {
+            do {
+                try {
+                    Message msg = new Message();
+                    msg.what = 1;  //消息(一个整型值)
+                    mHandler.sendMessage(msg);// 每隔1秒发送一个msg给mHandler
+                    Thread.sleep(35*1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            } while (true);
         }
     }
 }
